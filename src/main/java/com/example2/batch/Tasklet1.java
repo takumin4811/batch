5e86622fbfb819ff244cba5d317aea370aecb512
hogehoge <hoge@example.com>
2023-01-14 21:01:04 +0900

    first commit

---
+++ src/main/java/com/example2/batch/Tasklet1.java
@@ -0,0 +1,20 @@
+package com.example2.batch;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Tasklet1 implements Tasklet {
+
+  @Override
+  public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+      throws Exception {
+    System.out.println("Tasklet1 が実行されました。");
+    return RepeatStatus.FINISHED;
+  }
+
+}
